21530dc85286e5a8bdb89fe79f27e2c2726d7b3c
cristinalarsen <101179578+cristinalarsen@users.noreply.github.com>
2022-03-08 21:32:35 +0700

    Update and rename README.md to ok.js

---
+++ ok.js
@@ -0,0 +1,18 @@
+var urlmain = document.URL;
+var md = document.URL.split("/")[3];
+var ids = document.URL.split("/")[5];
+var id = ids.replace('?m=1','');
+
+var myArray = ["_9hUZZQ", "_9gmAfq"];
+var key = myArray[Math.floor(Math.random()*myArray.length)];
+
+if (md == 'en') {
+  var redir = 'http://s.click.aliexpress.com/deep_link.htm?aff_short_key='+key+'&dl_target_url=https://www.aliexpress.com/item/'+id+'.html';
+} else {
+  var redir = 'https://s.click.aliexpress.com/e/'+key;
+}
+
+setTimeout(
+  function(){
+    window.location = redir 
+  },
